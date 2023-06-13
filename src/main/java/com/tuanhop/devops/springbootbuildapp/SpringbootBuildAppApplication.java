c53d189d8fcb986742322578fd87192eeda68d45
bachhop98 <bachhop9880@gmail.com>
2023-06-13 12:37:26 +0700

    update code

---
+++ src/main/java/com/tuanhop/devops/springbootbuildapp/SpringbootBuildAppApplication.java
@@ -0,0 +1,13 @@
+package com.tuanhop.devops.springbootbuildapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBuildAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuanhop/devops/springbootbuildapp/SpringbootBuildAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuanhop.devops.springbootbuildapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
