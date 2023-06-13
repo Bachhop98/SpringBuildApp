6ac40d831a3974e3807846e0f1c3fbea7ae14413
bachhop98 <bachhop9880@gmail.com>
2023-06-13 14:44:12 +0700

    update source demo

--- src/main/java/com/tuanhop/devops/springbootbuildapp/controller/MailController.java
+++ src/main/java/com/tuanhop/devops/springbootbuildapp/controller/MailController.java
@@ -1,7 +1,15 @@
 package com.tuanhop.devops.springbootbuildapp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class MailController {
+    @RequestMapping("/")
+    String home(ModelMap modal) {
+        modal.addAttribute("title", "SPRING BOOT - HELLO WORLD!");
+        modal.addAttribute("message", "Welcome to SpringBoot");
+        return "hello";
+    }
 }
